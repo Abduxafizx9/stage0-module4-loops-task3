20c85e9afa822ad2fd06fedcd84b1366ad19a627
Abdulxafizx9 <abdulxafizx999@gmail.com>
2023-11-01 20:38:40 +0500

    Done

--- src/main/java/school/mjc/stage0/loops/task3/GreatestCommonDivisor.java
+++ src/main/java/school/mjc/stage0/loops/task3/GreatestCommonDivisor.java
@@ -2,26 +2,32 @@ package school.mjc.stage0.loops.task3;
 
 public class GreatestCommonDivisor {
     public void printGCD(int first, int second) {
-        if(first>=second)
+    //public static void main(String[] args) {
+        //int first = 100,second= 0;
+        int divider;
+        if(first == 0 || second == 0)
+            System.out.println(0);
+        else if(first>=second)
         {
-            for(;second<=1;second--) {
-                if (first % second == 0) {
-                    System.out.println(second);
+            divider=second;
+            for(;divider>=1;divider--) {
+                if (first % divider == 0 && second %divider==0) {
+                    System.out.println(divider);
                     break;
+                }
+            }}
+
+            else
+            {
+                divider=first;
+                for(;divider>=1;divider--) {
+                    if (second % divider == 0 && first %divider==0) {
+                        System.out.println(first);
+                        break;
+                    }
                 }
             }
 
         }
-        else if(first == 0 || second == 0)
-            System.out.println(0);
-        else
-        {
-            for(;first<=1;first--) {
-                if (second % first == 0) {
-                    System.out.println(first);
-                    break;
-                }
-            }
-        }
     }
-}
+
